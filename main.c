a2a8ce0381136bc75f5e8712eda2114ab11c7b9d
KhoaNguyen55 <HoangNKNguyen@proton.me>
2024-10-23 12:50:36 -0400

    add comments

--- main.c
+++ main.c
@@ -12,13 +12,16 @@
 
 #define CHILD_ID 0
 #define MAX_THREADS 5
-#define MAX_BYTES_PER_READ 10
+#define MAX_BYTES_PER_READ 1024
+
+// structs for data encapsulation
 
 struct chunkSize {
   long start;
   long end;
 };
 
+// each words and its associated count are implicit with the position
 struct WordsToFind {
   char **words;
   int *count;
@@ -40,41 +43,47 @@ struct Directory {
   int amount;
 };
 
+// function to format list of int to string
 void formatIntArray(char *output, int *list, int length) {
   int len = 0;
   len += sprintf(output + len, "[");
   for (int i = 0; i < length; i++) {
-    len += sprintf(output + len, "%i, ", list[i]);
+    len += sprintf(output + len, "%7i, ", list[i]);
   }
   sprintf(output + len - 2, "]");
 }
 
+// function to format list of string to string
 void formatStrArray(char *output, char **list, int length) {
   int len = 0;
   len += sprintf(output + len, "[");
   for (int i = 0; i < length; i++) {
-    len += sprintf(output + len, "%s, ", list[i]);
+    len += sprintf(output + len, "%7s, ", list[i]);
   }
   sprintf(output + len - 2, "]");
 }
 
+// function to read files inside a directory
 struct Directory readDirectory(char *directory) {
   struct Directory dir;
   dir.root = directory;
   dir.files = malloc(sizeof(char *));
   dir.amount = 0;
 
+  // open the folder
   DIR *dirStream = opendir(directory);
   if (dirStream == NULL) {
     fprintf(stderr, "Error open directory: %s\n", strerror(errno));
     exit(1);
   }
 
+  // loop through the files
   struct dirent *file = readdir(dirStream);
   int i = 1;
   while (file != NULL) {
     // ignore hidden files
     if (file->d_name[0] != '.') {
+      // add the file name to the array of files
       dir.files[dir.amount] = file->d_name;
       dir.amount++;
       if (dir.amount > i) {
@@ -93,6 +102,7 @@ struct Directory readDirectory(char *directory) {
   return dir;
 }
 
+// create a message queue
 mqd_t createMqQueue(char *queueName, int flags, const struct mq_attr *attr) {
   mqd_t queueId = mq_open(queueName, flags | O_CREAT, 0600, attr);
   if (queueId == -1) {
@@ -103,6 +113,7 @@ mqd_t createMqQueue(char *queueName, int flags, const struct mq_attr *attr) {
   return queueId;
 }
 
+// get the next word from the file
 int getWord(FILE *file, char *output, int maxSize) {
   char c = fgetc_unlocked(file);
   int i = 0;
@@ -118,24 +129,19 @@ int getWord(FILE *file, char *output, int maxSize) {
   return i;
 }
 
-/* Compare a word with words, if theres a match return the index of the word, -1
- * otherwise
- */
-int findWordLocation(struct WordsToFind *words, char *word) {
+// find a word and increment its count
+void findWordAndIncrement(struct WordsToFind *words, char *word) {
   for (int i = 0; i < words->length; i++) {
     if (strcmp(words->words[i], word) == 0) {
-      return i;
+      pthread_mutex_lock(&words->mutexes[i]);
+      words->count[i] += 1;
+      pthread_mutex_unlock(&words->mutexes[i]);
+      break;
     }
   }
-  return -1;
 }
 
-void incrementWordCount(struct WordsToFind *words, int location) {
-  pthread_mutex_lock(&words->mutexes[location]);
-  words->count[location] += 1;
-  pthread_mutex_unlock(&words->mutexes[location]);
-}
-
+// thread function for processing the text
 void *processText(void *args) {
   struct ThreadArgs *arg = args;
   FILE *file = fopen(arg->filePath, "r");
@@ -144,30 +150,34 @@ void *processText(void *args) {
       createMqQueue(arg->qName, O_RDONLY | O_NONBLOCK, arg->qAttr);
 
   char *word = malloc(100 * sizeof(char));
-  struct chunkSize chunk;
 
+  struct chunkSize chunk;
   while (1) {
+    // check if the parent thread want this thread to be terminated
     if (arg->terminate) {
       struct mq_attr attr;
       mq_getattr(threadQueue, &attr);
+      // if theres nothing in the queue then terminate
       if (attr.mq_curmsgs == 0) {
         break;
       }
     }
 
+    // get the next task in the queue
     if (mq_receive(threadQueue, (char *)&chunk, sizeof(struct chunkSize),
                    NULL) != -1) {
+      // seek to the chunk needed to scan
       if (fseek(file, chunk.start, SEEK_SET) != 0) {
         fprintf(stderr, "Error file are not seekable: %s\n", strerror(errno));
         exit(1);
       }
+      // read each word until the end of the chunk
       while (ftell(file) < chunk.end) {
         int size = getWord(file, word, 100);
         if (size > 0) {
-          int location = findWordLocation(arg->words, word);
-          if (location >= 0) {
-            incrementWordCount(arg->words, location);
-          }
+          // if a word is found then increment it
+          // if its in the words we need to find
+          findWordAndIncrement(arg->words, word);
         } else if (size == EOF) {
           break;
         }
@@ -177,6 +187,7 @@ void *processText(void *args) {
     }
   }
 
+  // clean up
   free(word);
   pthread_exit(0);
 }
@@ -185,9 +196,12 @@ void *processText(void *args) {
 // read, `errno = EOF` otherwise
 struct chunkSize getNextChunkPosition(FILE *file, long chunkSize) {
   struct chunkSize chunk;
+  // get the start of the chunk
   chunk.start = ftell(file);
   fseek(file, chunkSize - 1, SEEK_CUR);
 
+  // look for a space, newline or end of line, to seperate the chunk
+  // this is to make sure the chunk don't cut off a word midway
   char c = fgetc(file);
   while (c != ' ' && c != '\n' && c != EOF) {
     fputc(c, file);
@@ -207,23 +221,29 @@ struct chunkSize getNextChunkPosition(FILE *file, long chunkSize) {
   return chunk;
 }
 
+// process each file by creating threads for it
 void processFile(struct WordsToFind words, char *root, char *fileName) {
+  // get the pid to create a inter thread communication
   pid_t pid = getpid();
   char *qName = malloc(sizeof(char[256]));
+  // make sure the queue name don't collide with other threads
   snprintf(qName, 255, "/thread%i", pid);
   mq_unlink(qName);
 
+  // create the queue
   const struct mq_attr tAttr = {0, words.length, sizeof(struct chunkSize), 0};
   mqd_t threadQueue = createMqQueue(qName, O_WRONLY, &tAttr);
 
   pthread_t threads[MAX_THREADS];
 
+  // add in the arguments for the thread function
   char *filePath = malloc(2 * sizeof(char[256]));
   strcpy(filePath, root);
   strcat(filePath, fileName);
 
   struct ThreadArgs arg = {&words, filePath, &tAttr, qName, 0};
 
+  // create the threads
   for (int i = 0; i < MAX_THREADS; i++) {
     pthread_create(&threads[i], NULL, processText, (void *)&arg);
   }
@@ -231,6 +251,7 @@ void processFile(struct WordsToFind words, char *root, char *fileName) {
   FILE *file = fopen(filePath, "r");
   struct chunkSize tMsg;
   do {
+    // get the next chunk and put it in the queue
     tMsg = getNextChunkPosition(file, MAX_BYTES_PER_READ);
     if (mq_send(threadQueue, (char *)&tMsg, sizeof(tMsg), 0) != 0) {
       fprintf(stderr, "Error sending queue to thread: %s\n", strerror(errno));
@@ -238,22 +259,27 @@ void processFile(struct WordsToFind words, char *root, char *fileName) {
     }
   } while (errno != EOF);
 
+  // turn on flag to terminate all the thread when its done
   arg.terminate = 1;
 
+  // join to make sure its all done
   for (int i = 0; i < MAX_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
 
+  // create queue to send all the data back to the main process
   const int pMsgSize = words.length * sizeof(int);
   const struct mq_attr pAttr = {0, 10, sizeof(pMsgSize), 0};
   mqd_t processQueue = createMqQueue("/process", O_WRONLY, &pAttr);
 
+  // send the data
   if (mq_send(processQueue, (char *)words.count, pMsgSize, 0) != 0) {
     fprintf(stderr, "Error sending word count to process: %s\n",
             strerror(errno));
     exit(1);
   }
 
+  // clean up
   free(filePath);
   mq_close(processQueue);
   mq_close(threadQueue);
@@ -261,11 +287,15 @@ void processFile(struct WordsToFind words, char *root, char *fileName) {
   free(qName);
 }
 
+// read all the files and fork a new process for each file
 void readFiles(struct WordsToFind words, struct Directory dir, int offset) {
   if (dir.amount <= offset) {
+    // clean up since we don't need it anymore
     free(dir.files);
     return;
   }
+
+  // fork a new process and process each files
   switch (fork()) {
   case -1:
     fprintf(stderr, "Error forking directory: %s\n", strerror(errno));
@@ -274,16 +304,18 @@ void readFiles(struct WordsToFind words, struct Directory dir, int offset) {
     processFile(words, dir.root, dir.files[offset]);
     exit(0);
   default:
+    // recursive loop
     readFiles(words, dir, offset + 1);
   }
 }
 
 int main(void) {
+  // initalizing all the variables needed
+  char *tempwords[] = {"void", "the", "a", "machine", "thing"};
+  const int wordsToFindLength = 5;
+  char directory[] = "./files/";
+
   mq_unlink("/process");
-
-  char *tempwords[] = {"101", "hi"};
-  const int wordsToFindLength = 2;
-
   char **words = malloc(wordsToFindLength * sizeof(char *));
   for (int i = 0; i < wordsToFindLength; i++) {
     words[i] = tempwords[i];
@@ -301,10 +333,13 @@ int main(void) {
 
   struct WordsToFind wordsToFind = {words, count, mutexes, wordsToFindLength};
 
-  struct Directory dir = readDirectory("./test/");
+  // read the directory
+  struct Directory dir = readDirectory(directory);
 
+  // process the files
   readFiles(wordsToFind, dir, 0);
 
+  // initialize data for receiving
   const int msgSize = wordsToFindLength * sizeof(int);
   const struct mq_attr pAttr = {0, 10, msgSize, 0};
   mqd_t queueId = createMqQueue("/process", O_RDONLY | O_NONBLOCK, &pAttr);
@@ -312,8 +347,10 @@ int main(void) {
   int *wordsFound = malloc(msgSize);
   int deadProcesses = 0;
   while (1) {
+    // get the words count
     if (mq_receive(queueId, (char *)wordsFound, msgSize, NULL) != -1) {
       for (int i = 0; i < wordsToFindLength; i++) {
+        // add it to the total;
         wordsToFind.count[i] += wordsFound[i];
       }
     } else if (errno == EAGAIN && waitpid(-1, NULL, WNOHANG) != 0) {
@@ -322,19 +359,22 @@ int main(void) {
       printf("Error process mq_receive: %s\n", strerror(errno));
     }
 
+    // check to make sure all process are killed
     if (deadProcesses == dir.amount) {
       break;
     }
   }
 
+  // print the words and it's counts
   char *outputStr = malloc(100 * sizeof(char));
   formatStrArray(outputStr, wordsToFind.words, wordsToFind.length);
   char *outputInt = malloc(100 * sizeof(char));
   formatIntArray(outputInt, wordsToFind.count, wordsToFind.length);
   printf("Words: %s \nFound: %s \n", outputStr, outputInt);
+
+  // clean up
   free(outputInt);
   free(outputStr);
-
   free(wordsFound);
   free(words);
   mq_close(queueId);
